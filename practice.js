c240524e807c99c816109e86f395b6e1ffc5ffdf
amrit raj <amritrajsingh179@gmail.com>
2025-05-25 16:29:59 +0530

    practice

---
+++ practice.js
@@ -0,0 +1,34 @@
+// calculate sum of first n number
+
+// let n=5;
+// let ans=0
+// ans+=n*(n+1)/2;
+// console.log(ans);
+
+
+
+
+// calculate the sum of number from m to n
+
+// let m=4;
+// let n=5;
+// let ans=0;
+// for(let i=m;i<=n;i++){
+// ans+=i;
+// }
+// console.log(ans);
+
+
+
+
+// print all the odd number from 0 to n
+
+// let n = 10;
+// for(let i=1;i<=n;i++){
+//     if (i%2!==0){
+//         console.log(i);
+        
+//     }
+// }
+
+

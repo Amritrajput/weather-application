bc02e67d43d8d9652c8aa22103e0e8ea14a0f176
amrit raj <amritrajsingh179@gmail.com>
2025-05-25 17:24:05 +0530

    textbasedgame

--- practice.js
+++ practice.js
@@ -32,3 +32,26 @@
 // }
 
 
+// wap in js to create a factorial of 'n'.
+
+// let n=5
+// let fact=1
+// for(let i =n ;i>=1;i--){
+// fact*=i;
+// }
+// console.log(fact);
+
+// print 
+// 1
+// 1 2 
+// 1 2 3
+// 1 2 3 4 
+// 1 2 3 4 5 
+
+// for(let i=1;i<=5;i++){
+//     let str= ""
+//     for(let j=1;j<=i;j++){
+//     str = str+" "+j;
+//     }
+//     console.log(str);
+// }
